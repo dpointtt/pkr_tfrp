a4edd680e73644fbd527a26360feabb0831e05b2
dpointtt <dpointttwink@gmail.com>
2024-11-20 00:51:22 +0200

    Initial commit

---
+++ src/main/java/ua/com/dpointtt/pkr_tfrp/PkrTfrpApplication.java
@@ -0,0 +1,13 @@
+package ua.com.dpointtt.pkr_tfrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkrTfrpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkrTfrpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/com/dpointtt/pkr_tfrp/PkrTfrpApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.dpointtt.pkr_tfrp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkrTfrpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
